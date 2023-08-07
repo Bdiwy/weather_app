7c8144b8f03f2773f49d48e8ac2ea31ce3fff905
Ahmed Bdiwy <ahmedbedawi22@gmail.com>
2023-08-07 14:00:42 +0300

    last touch

--- weather/views.py
+++ weather/views.py
@@ -8,18 +8,15 @@ def index(request):
         city = request.POST['city']
         res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=97f476dee0cf24a3fd18a19988b3242c').read()
         json_data = json.loads(res)
-        data={
-            'country': str(json_data['sys']['country']),
-            'cordinates': str(json_data['coord']['lon'])+''+str(json_data['coord']['lat']),
-            'temp':str( json_data['main']['temp'])+'k',
-            'pressure':str(json_data['main']['pressure']),
-            'humidity':str(json_data['main']['humidity']),
-
-        }
-    else:
-        city = ''   
-        data={}
-        res=''
-        return redirect('index')
+        data ={
+             'country': str(json_data['sys']['country']),
+             'cordinates': str(json_data['coord']['lon'])+''+str(json_data['coord']['lat']),
+             'temp':str( json_data['main']['temp'])+'k',
+             'pressure':str(json_data['main']['pressure']),
+             'humidity':str(json_data['main']['humidity']),
  
-    return render(request, 'index.html',{"data":data , "city":city})    
+          }
+    else:
+        city=''
+        data={}    
+    return render(request, 'index.html',{ "data":data ,"city":city})    
